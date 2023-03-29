ba14642150aa7e07a260c2421d8c3cdb79516c66
kulanjith <devin.gallage@clouda.io>
2023-03-29 11:53:47 +0530

    test test

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import inquirer
+import os
+import subprocess
 
 change = inquirer.list_input("type of change", choices=['major', 'minor', 'patch'])
 
+bashCommand = "git tag --sort=committerdate | grep -E '[0-9]' | tail -1 | cut -b 2-7"
+pro = subprocess.Popen(bashCommand, stdout= subprocess.PIPE, shell=True)
+pro.wait()
+result = pro.stdout.readlines()
+for tag in result:
+    decodeTag = tag.decode("utf-8")
+    splitTag = decodeTag.split('.')
+
+    major = int(splitTag[0])
+    minor = int(splitTag[1])
+    patch = int(splitTag[2])
+
+    global newTag 
+
+    if change == 'major':
+        newTag = str(major+1)+"."+"0"+"."+"0"
+  
+    if change == 'minor':
+        newTag = str(major)+"."+str(minor+1)+"."+"0"
+       
+    if change == 'patch':
+        newTag = str(major)+"."+str(minor)+"."+str(patch+1)
+
+    print(newTag)
